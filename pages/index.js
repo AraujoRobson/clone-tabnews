a8e956d8e1e7a0bf4a0c4152483518c8d668376b
Robson Araujo <44716944+AraujoRobson@users.noreply.github.com>
2023-09-22 20:40:29 +0000

    corrige frase

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
     e cara eu to feliz pra caramba de vc estar vendo isso, obrigado irmão ❤️<br />
     <br />
     esse era o primeiro desafio do curso do Deschamps<br />
-    e o desafio tinha que pensar em alguem para você surepeender<br />
+    e o desafio tinha que pensar em alguem para eu surepeender<br />
     e nao tinha como nao ser você brow, que me ajuda pra caralho.<br />
   </h1>
 }
