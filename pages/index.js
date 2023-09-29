bd89a6c35cca5a14d28d0e8d77630f174307cb5f
Robson Araujo <44716944+AraujoRobson@users.noreply.github.com>
2023-09-29 14:23:33 +0000

    atualiza pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Brow, queria agradecer a tua dedicação comigo,
-      <br />
-      essa é minha primeira pagina disponivel na web
-      <br />
-      e cara eu to feliz pra caramba de vc estar vendo isso, obrigado irmão ❤️
-      <br />
-      <br />
-      esse era o primeiro desafio do curso do Deschamps
-      <br />
-      e o desafio tinha que pensar em alguem para eu surepeender
-      <br />
-      e nao tinha como nao ser você brow, que me ajuda pra caralho.
-      <br />
-    </h1>
-  );
+  return <h1>Página em Construção!!</h1>;
 }
 
 export default Home;
